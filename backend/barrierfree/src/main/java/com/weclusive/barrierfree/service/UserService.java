7b7e0a8dcae40817a76f4fdccef00558c97e2df3
sdi1358_ <sdi1358_@naver.com>
2022-01-24 13:23:45 +0900

    Feat: 회원가입 및 이메일 인증 구현
    회원가입, 이메일 인증 개발
    
    ref: #10, #14

--- backend/barrierfree/src/main/java/com/weclusive/barrierfree/service/UserService.java
+++ backend/barrierfree/src/main/java/com/weclusive/barrierfree/service/UserService.java
@@ -2,9 +2,22 @@ package com.weclusive.barrierfree.service;
 
 import java.util.List;
 
-import com.weclusive.barrierfree.dto.*;
+import com.weclusive.barrierfree.entity.*;
 
 public interface UserService {
 
-	public List<User> allUsers();
+	public void registUser(User user);
+
+//	public List<User> allUsers();
+
+	public User findByUserId(String userId);
+
+	public User findByUserNickname(String userNickname);
+
+	public void sendEmailwithUserKey(String email, String id);
+
+	public User email_cert_check(String userNickname);
+
+	public void email_certified_update(User user);
+
 }
