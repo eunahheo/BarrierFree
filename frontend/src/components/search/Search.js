0307486032901d1d742e2c8d8eef64740bc20418
jeonghyun <chance213213@gmail.com>
2022-01-28 10:11:40 +0900

    Fix: 디렉토리 구조 변경
    마스터 브랜트와 합병을 위한 디렉터리 구조 변경 진행

---
+++ frontend/src/components/search/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainCard from "../common/MainCard.js"
+
+function Search() {
+  return (
+    <div>
+      <h3>검색하기</h3>
+      <hr></hr>
+      <MainCard></MainCard>
+    </div>
+  )
+}
+
+export default Search;
