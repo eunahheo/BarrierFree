ffecc258aeee505717fcc83d1278595611fd7184
sdi1358_ <sdi1358_@naver.com>
2022-02-09 22:53:36 +0900

    feat: "로그인/회원가입 CSS"
    로그인, 회원가입 CSS 수정
    
    #Resolves: #179, 185

--- frontend/src/containers/auth/RegisterForm.js
+++ frontend/src/containers/auth/RegisterForm.js
@@ -1,17 +1,17 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
-import AuthForm from "../../components/auth/AuthForm";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import AuthForm from '../../components/auth/AuthForm';
 
 const RegisterForm = () => {
   const [pwdCfm, setPwdCfm] = useState(true);
   const navigate = useNavigate();
   const [registerloading, setRegisterloading] = useState(false);
   const [regform, setForm] = useState({
-    userId: "",
-    userEmail: "",
-    userPwd: "",
-    userNickname: "",
+    userId: '',
+    userEmail: '',
+    userPwd: '',
+    userNickname: '',
     physical: 0,
     visibility: 0,
     infant: 0,
@@ -56,39 +56,39 @@ const RegisterForm = () => {
       EnableuserNickname,
     } = regform;
 
-    console.log("닉네임 검사:", nicknameExp.test(userNickname));
-    console.log("아이디 유효성 검사::", idExp.test(userId));
-    console.log("비밀번호 유효성 검사::", regExp.test(userPwd));
+    console.log('닉네임 검사:', nicknameExp.test(userNickname));
+    console.log('아이디 유효성 검사::', idExp.test(userId));
+    console.log('비밀번호 유효성 검사::', regExp.test(userPwd));
     if (idExp.test(userId) === false) {
-      alert("아이디는 영어, 숫자만 가능합니다");
+      alert('아이디는 영어, 숫자만 가능합니다');
       return;
     }
     if (userId.length < 5 || userId.length > 20) {
-      alert("아이디는 5~20자 까지만 가능합니다.");
+      alert('아이디는 5~20자 까지만 가능합니다.');
       return;
     }
     if (regExp.test(userPwd) === false) {
-      alert("비밀번호는 영어, 숫자, !@#$%^&*를 포함해야합니다.");
+      alert('비밀번호는 영어, 숫자, !@#$%^&*를 포함해야합니다.');
       return;
     }
     if (userPwd.length < 8 || userPwd.length > 16) {
-      alert("비밀번호는 8자~16자까지만 가능합니다.");
+      alert('비밀번호는 8자~16자까지만 가능합니다.');
       return;
     }
     if (nicknameExp.test(userNickname) === false) {
-      alert("닉네임은 한글,영어,숫자만 가능합니다.");
+      alert('닉네임은 한글,영어,숫자만 가능합니다.');
       return;
     }
     if (userNickname.length < 2 || userNickname.length > 6) {
-      alert("닉네임은 2~8자까지만 가능합니다.");
+      alert('닉네임은 2~8자까지만 가능합니다.');
       return;
     }
     if (EnableuserId === false) {
-      alert("아이디 중복 여부를 확인해주세요");
+      alert('아이디 중복 여부를 확인해주세요');
       return;
     }
     if (EnableuserNickname === false) {
-      alert("닉네임 중복 여부를 확인해주세요");
+      alert('닉네임 중복 여부를 확인해주세요');
       return;
     }
     if (
@@ -113,21 +113,21 @@ const RegisterForm = () => {
       setRegisterloading(true);
       try {
         await axios({
-          url: "http://i6a504.p.ssafy.io:3030/user/join/",
-          method: "post",
+          url: 'http://i6a504.p.ssafy.io:3030/user/join/',
+          method: 'post',
           headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Credentials": true,
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Credentials': true,
           },
           data: regform,
         });
-        alert("회원가입이 완료되었습니다!😀");
-        navigate("/registerpage/emailcheck");
+        alert('회원가입이 완료되었습니다!😀');
+        navigate('/registerpage/emailcheck');
       } catch (error) {
         console.log(error);
       }
     } else {
-      alert("빈 값을 채워주세요!");
+      alert('빈 값을 채워주세요!');
     }
   };
 
