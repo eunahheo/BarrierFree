dffc111731f34509eea224b15f6deab13b55271d
jeonghyun <chance213213@gmail.com>
2022-02-09 15:48:18 +0900

    Feat: /app/build/logo192.png

--- frontend/src/routes/About.js
+++ frontend/src/routes/About.js
@@ -5,6 +5,7 @@ function About() {
     <div>
       <span>About weclusive</span>;
       <img src="/app/2022/02/09/896bda0b-b73a-4b38-8ee2-397e44301f78_1help.jpg"></img>
+      <img src="/app/build/logo192.png"></img>
     </div>
   );
 }
