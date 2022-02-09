2ce74ba523d514fed77649b1b4269192e8041d30
jeonghyun <chance213213@gmail.com>
2022-02-09 14:40:56 +0900

    Test: nginx test

--- frontend/src/routes/About.js
+++ frontend/src/routes/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div>
       <span>About weclusive</span>;
-      <img src="./app/2022/02/09/896bda0b-b73a-4b38-8ee2-397e44301f78_1help.jpg"></img>
+      <img src="/app/2022/02/09/896bda0b-b73a-4b38-8ee2-397e44301f78_1help.jpg"></img>
     </div>
   );
 }
