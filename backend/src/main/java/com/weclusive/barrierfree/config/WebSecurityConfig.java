1f4de72c6b5cda6a7a546b9c0a1f1a69648756d7
유민상 <yminsang96@gmail.com>
2022-02-10 16:42:17 +0000

    Update WebSecurityConfig.java

--- backend/src/main/java/com/weclusive/barrierfree/config/WebSecurityConfig.java
+++ backend/src/main/java/com/weclusive/barrierfree/config/WebSecurityConfig.java
@@ -74,7 +74,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://i6a504.p.ssafy.io/api");
+        configuration.addAllowedOrigin("http://i6a504.p.ssafy.io");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
