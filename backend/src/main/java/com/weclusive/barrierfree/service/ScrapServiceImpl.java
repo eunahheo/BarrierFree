b375623cc3692f4ef42547bfa729caeb1fc431a0
blue8959 <blue8959@naver.com>
2022-02-13 10:17:56 +0900

    Feat: "추가 스크랩 취소"
    스크랩 취소하기 기능 추가
    스크랩 추가하기 시 사용자 아이디 추가

--- backend/src/main/java/com/weclusive/barrierfree/service/ScrapServiceImpl.java
+++ backend/src/main/java/com/weclusive/barrierfree/service/ScrapServiceImpl.java
@@ -2,12 +2,15 @@ package com.weclusive.barrierfree.service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.weclusive.barrierfree.entity.Scrap;
+import com.weclusive.barrierfree.entity.User;
 import com.weclusive.barrierfree.repository.ScrapRepository;
+import com.weclusive.barrierfree.repository.UserRepository;
 import com.weclusive.barrierfree.util.TimeUtils;
 
 @Service
@@ -15,27 +18,23 @@ public class ScrapServiceImpl implements ScrapService {
 
 	@Autowired
 	ScrapRepository scrapRepository;
-	
+
+	@Autowired
+	UserRepository userRepository;
+
 	@Override
-	public Map<String, Integer> insertScrap(int user_seq, char scrap_type, long scrap_data) throws Exception{
+	public Map<String, Integer> insertScrap(int user_seq, char scrap_type, long scrap_data) throws Exception {
 		Map<String, Integer> result = new HashMap<>();
 		String regTime = TimeUtils.curTime();
 		try {
-		scrapRepository.save(Scrap.builder()
-				.userSeq(user_seq)
-				.scrapType(scrap_type)
-				.scrapData(scrap_data)
-				.regDt(regTime)
-				.regId("임시")
-				.modDt(regTime)
-				.modId("임시").build());
-		result.put("result",1);
-		}
-		catch(Exception e) {
+			scrapRepository.save(Scrap.builder().userSeq(user_seq).scrapType(scrap_type).scrapData(scrap_data)
+					.regDt(regTime).regId(returnUserId(user_seq)).modDt(regTime).modId(returnUserId(user_seq)).build());
+			result.put("result", 1);
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception();
 		}
-		
+
 		return result;
 	}
 
@@ -44,25 +43,46 @@ public class ScrapServiceImpl implements ScrapService {
 		int result = 0;
 		try {
 			result = scrapRepository.countByDelYnAndScrapTypeAndScrapData('n', scrap_type, scrap_data);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			throw new Exception();
-		}
-		return result;
-	}
-	
-	@Override
-	public int getScrapYn(int userSeq, char scrapType, long scrapData) throws Exception {
-		int result = 0;
-		try {
-			result = scrapRepository.countByDelYnAndScrapTypeAndUserSeqAndScrapData('n', scrapType, userSeq, scrapData);
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception();
 		}
 		return result;
 	}
 
+	@Override
+	public int getScrapYn(int userSeq, char scrapType, long scrapData) throws Exception {
+		int result = 0;
+		try {
+			result = scrapRepository.countByDelYnAndScrapTypeAndUserSeqAndScrapData('n', scrapType, userSeq, scrapData);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception();
+		}
+		return result;
+	}
+
+	// 게시글 삭제하기 (del_yn을 y로 변경)
+	@Override
+	public Optional<Scrap> deleteByScrapSeq(long scrapSeq, int userSeq) {
+		Optional<Scrap> deleteScrap = scrapRepository.findByScrapSeqAndDelYn(scrapSeq, 'n');
+
+		if (deleteScrap != null) {
+			String curTime = TimeUtils.curTime();
+
+			deleteScrap.get().setDelYn('y');
+			deleteScrap.get().setModDt(curTime);
+			deleteScrap.get().setModId(returnUserId(userSeq));
+			scrapRepository.save(deleteScrap.get());
+			return deleteScrap;
+		} else
+			return null;
+	}
+
+	// userSeq -> userId
+	public String returnUserId(int userSeq) {
+		Optional<User> list = userRepository.findById(userSeq);
+		String userId = list.get().getUserId();
+		return userId;
+	}
 }
