57ab3057982646d25b184be76cb931b909ef0117
blue8959 <blue8959@naver.com>
2022-02-17 00:04:29 +0900

    Design: "추가 about 페이지"
    about 페이지 추가

--- frontend/src/components/about/team.js
+++ frontend/src/components/about/team.js
@@ -9,7 +9,8 @@ const T = styled.div`
     box-shadow: 0 0 50px rgba(0, 0, 0, 0.06);
   }
   #services {
-    padding: 100px;
+    padding: 20px 100px;
+    position: relative;
   }
   .row {
     display: flex;
@@ -35,55 +36,105 @@ const T = styled.div`
     color: #fff;
     box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.05);
   }
-  #services h3 {
-    font-weight: 500;
-    padding: 5px 0;
-    color: #000;
-  }
+
   #services p {
     color: black;
     font-size: 20px;
   }
   span {
     color: #ea5455;
-    // font-size: 35px;
   }
 
   .left {
     flex: 5;
   }
-  // .center {
-  //   flex: 3;
-  // }
   .right {
-    flex: 4;
+    flex: 5;
+    display: flex;
   }
-  p {
+  .content {
     font-size: 20px;
-    font-weight: bold;
+    line-height: 40px;
+  }
+  .p {
+    padding-top: 30px;
+  }
+  #row2 {
+    align-items: center;
+    align-content: center;
+    justify-content: center;
   }
 `;
 export const Team = () => {
   return (
     <T>
       <div id="services" className="text-center">
-        <div className="row">
-          <div className="left">
-            <img src="Weclusive.png" width="90%"></img>
-          </div>
-          {/* <div className="center">
-            <h1>Weclusive</h1>
-            <p>= We + Inclusive</p>
-            <br></br>
-            <p>우리라는 We와 모든 것을 포함하는 Inclusive의 합성어</p>
-            <br></br>
-            <h1>mission</h1>
-            <p> 사회적 약자들도 수용하는 세상을 만들어가자</p>
-          </div> */}
-          <div className="right">
-            <img src="we.png" width="100%"></img>
+        <img
+          src="weclusive.png"
+          width="50%"
+          style={{ paddingTop: '50px' }}
+        ></img>
+        <div className="row" id="row2">
+          <div id="services" className="text-center">
+            <div className="p">
+              <h2 style={{ textAlign: 'left' }}> 우리의 대표 색</h2>
+              <div className="row">
+                <div className="left">
+                  <img src="colors.png" width="500px"></img>
+                </div>
+                <div
+                  className="right"
+                  style={{
+                    paddingLeft: '50px',
+                    paddingRight: '50px',
+                  }}
+                >
+                  <div className="content">
+                    색약, 색맹 질환을 가진 색각 이상자분들도<br></br>
+                    쉽게 구분할 수 있는 4가지 색을 대표색으로 지정 했고,
+                    <br></br> 이 대표색을 이용하여 홈페이지를 구성했습니다.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p">
+              <h2 style={{ textAlign: 'right', paddingRight: '70px' }}>
+                우리의 아이콘
+              </h2>
+              <div className="row">
+                <div
+                  className="left"
+                  style={{
+                    paddingLeft: '50px',
+                    paddingRight: '50px',
+                  }}
+                >
+                  <div className="content" style={{ textAlign: 'right' }}>
+                    색 뿐만 아니라 빗금을 사용해서 선택 유무를 표현했고,{' '}
+                    <br></br>
+                    사용자분들이 편리하게 이용할 수 있도록 했습니다.
+                  </div>
+                </div>
+                <div className="right">
+                  <img
+                    src="icon.png"
+                    height="100px"
+                    style={{ paddingLeft: '20px' }}
+                  ></img>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="p">
+                <h2 style={{ textAlign: 'left' }}>기능</h2>
+                <div className="content" style={{ textAlign: 'left' }}>
+                  사진을 클릭하면 사진에 대한 정보를 읽어줍니다.
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <img src="we.png" width="50%"></img>
       </div>
     </T>
   );
