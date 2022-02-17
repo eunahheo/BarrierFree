e33e32f6679d4f4e641b430d73d9e57d579c34b0
blue8959 <blue8959@naver.com>
2022-02-17 17:22:23 +0900

    Design: "추가 이번주 인기글"
    이번주 인기글

--- frontend/src/components/common/review/Carousel.js
+++ frontend/src/components/common/review/Carousel.js
@@ -4,16 +4,54 @@ import 'react-multi-carousel/lib/styles.css';
 import './style.css';
 import WithScrollbar from './WithScrollbar.js';
 import Section from './Section.js';
+import styled from 'styled-components';
+
+const B = styled.div`
+  // display: grid;
+  // gap: 1ch;
+  .textShadow {
+    text-shadow: 0.1em 0.1em 0 #e5e5e5;
+  }
+  // .center {
+  //   // position: absolute;
+  //   // left: 50%;
+  //   // margin: 0 auto;
+  // }
+  h1 {
+    font-size: 3rem;
+    font-weight: 900;
+    line-height: 1.1;
+    color: #2d4059;
+
+    // place-content: center;
+  }
+`;
 
 function SwipeableTextMobileStepper({ myWeeklyList, myuser }) {
   return (
-    <Box sx={{ maxWidth: 1300, flexGrow: 1, overflow: 'hidden' }}>
-      <Fragment sx={{ maxWidth: 1300, flexGrow: 1 }}>
-        <Section>
-          <WithScrollbar myWeeklyList={myWeeklyList} myuser={{ myuser }} />
-        </Section>
-      </Fragment>
-    </Box>
+    <B>
+      <Box sx={{ maxWidth: 1300, flexGrow: 1, overflow: 'hidden' }}>
+        <Fragment sx={{ maxWidth: 1300, flexGrow: 1 }}>
+          <div className="center">
+            <h1 className="textShadow">이번주 인기글</h1>
+          </div>
+          {/* <div className="title">
+            <div class="waviy">
+              <span style="--i:1">이</span>
+              <span style="--i:2">번</span>
+              <span style="--i:3">주</span>
+              <span style="--i:4"> </span>
+              <span style="--i:5">인</span>
+              <span style="--i:6">기</span>
+              <span style="--i:7">글</span>
+            </div>
+          </div> */}
+          <Section>
+            <WithScrollbar myWeeklyList={myWeeklyList} myuser={{ myuser }} />
+          </Section>
+        </Fragment>
+      </Box>
+    </B>
   );
 }
 
